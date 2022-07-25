f7b941d5a4dd525aecf5f3e7094116775471952d
AntonioMohallemAoun <109971245+AntonioMohallemAoun@users.noreply.github.com>
2022-07-25 11:54:02 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(4000,4000);
+  var bbbbbbbb = createSprite(20, 20, 20, 20);
+  
+  bbbbbbbb.shapeColor = "white";
+}
+
+function draw() 
+{
+
+  background(30);
+  drawSprites();
+}
+
+
+
+
